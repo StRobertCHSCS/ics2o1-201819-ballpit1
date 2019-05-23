b4e33684107e503759370e07992ecf15aa1ff11e
jason <jason.tai21@ycdsbk12.ca>
2019-05-23 13:47:54 -0400

    Arcade poster

--- SimpleArcade/player_movement.py
+++ SimpleArcade/player_movement.py
@@ -1,12 +1,18 @@
 import arcade
+import time
 
-
+then = time.time()
 WIDTH = 640
 HEIGHT = 480
 
+score = 0
+
+kelvin = arcade.load_texture("Screenshot-20181016-190732.PNG")
+kristen = arcade.load_texture("IMG-2607.JPG")
+
 # start player position in middle of window
-player_x = WIDTH/2
-player_y = HEIGHT/2
+player_x = WIDTH / 2
+player_y = HEIGHT / 2
 
 # Variables to record if certain keys are being pressed.
 up_pressed = False
@@ -14,43 +20,153 @@ down_pressed = False
 left_pressed = False
 right_pressed = False
 
+# moving block
+center_x = 100  # Initial x position
+center_y = 50  # Initial y position
+delta_x = 3  # change in x
+delta_y = 1  # change in y
+
+RECT_WIDTH = 50
+RECT_HEIGHT = 50
+
+#  block to avoid
+my_button = [100, 200, 150, 50]
+
+dead = False
+
+
+def on_collision():
+    global player_x, player_y, dead, center_y, center_x, score
+    if 75 <= player_x <= 275 and 175 <= player_y <= 275:
+        print("PLayer died")
+        dead = True
+
+
+    if center_x - 45 <= player_x <= center_x + 45 and center_y - 45 <= player_y <= center_y + 45:
+        print("PLayer died")
+        dead = True
+
+
+
+    if dead:
+        print("score is ", score)
+    else:
+        if center_x - 100 <= player_x <= center_x + 100 and center_y - 100 <= player_y <= center_y + 100:
+            score += 1
+        if center_x - 80 <= player_x <= center_x + 80 and center_y - 80 <= player_y <= center_y + 80:
+            score += 2
+
 
 def on_update(delta_time):
-    global up_pressed, player_y
+    global up_pressed, player_y, down_pressed, right_pressed, left_pressed, player_x
     if up_pressed:
         player_y += 5
+    if down_pressed:
+        player_y -= 5
+    if right_pressed:
+        player_x += 5
+    if left_pressed:
+        player_x -= 5
+    on_collision()
 
+    global center_x, center_y
+    global delta_x, delta_y
+
+    center_x += delta_x
+    center_y += delta_y
+
+    # Figure out if we hit the edge and need to reverse.
+    if center_x < RECT_WIDTH // 2 or center_x > WIDTH - RECT_WIDTH // 2:
+        delta_x *= -1.1
+        if delta_x < 0:
+            print(delta_x * -1, "is the current speed")
+        else:
+            print(delta_x, "is the current speed")
+
+    if center_y < RECT_HEIGHT // 2 or center_y > HEIGHT - RECT_HEIGHT // 2:
+        delta_y *= -1.1
+
+    if player_x - 25 <= 0:
+        player_x = 25
+        print("YEET")
+
+    if player_x + 25 >= WIDTH:
+        player_x = WIDTH - 25
+
+    if player_y - 25 <= 0:
+        player_y = 25
+
+    if player_y + 25 >= HEIGHT:
+        player_y = HEIGHT - 25
 
 def on_draw():
-    global player_x, player_y
+    global player_x, player_y, dead, now, printed
     arcade.start_render()
     # Draw in here...
-    arcade.draw_circle_filled(player_x, player_y, 25, arcade.color.BLUE)
+    arcade.draw_circle_filled(player_x, player_y, 25, arcade.color.RED)
+    arcade.draw_texture_rectangle(player_x, player_y, 0.069*721, 0.069*801, kristen,0)
+
+    # Draw in here...
+    arcade.draw_xywh_rectangle_filled(my_button[0],
+                                      my_button[1],
+                                      my_button[2],
+                                      my_button[3],
+                                      arcade.color.BLACK)
+    arcade.draw_rectangle_filled(center_x, center_y, RECT_WIDTH, RECT_HEIGHT, arcade.color.BLUE)
+    arcade.draw_texture_rectangle(center_x, center_y, 0.169*295, 0.169*305, kelvin, 0)
+
+    arcade.draw_text("Score: "+str(score), 100, 50, arcade.color.BLACK)
+
+    if dead:
+        arcade.set_background_color(arcade.color.RED)
+        arcade.draw_text("You died, your score is shit " + str(score), 100, HEIGHT - 100, arcade.color.BLACK, 30)
+        now = time.time()
+        print(now - then, "seconds")
+
+
+
 
 
 def on_key_press(key, modifiers):
     global up_pressed
     if key == arcade.key.W:
         up_pressed = True
+    global down_pressed
+    if key == arcade.key.S:
+        down_pressed = True
+    global right_pressed
+    if key == arcade.key.D:
+        right_pressed = True
+    global left_pressed
+    if key == arcade.key.A:
+        left_pressed = True
 
 
 def on_key_release(key, modifiers):
     global up_pressed
     if key == arcade.key.W:
         up_pressed = False
-
-
-def on_mouse_press(x, y, button, modifiers):
-    pass
+    global down_pressed
+    if key == arcade.key.S:
+        down_pressed = False
+    global right_pressed
+    if key == arcade.key.D:
+        right_pressed = False
+    global left_pressed
+    if key == arcade.key.A:
+        left_pressed = False
 
 
 def setup():
+
     arcade.open_window(WIDTH, HEIGHT, "My Arcade Game")
-    arcade.set_background_color(arcade.color.WHITE)
-    arcade.schedule(on_update, 1/60)
+
+    arcade.set_background_color(arcade.color.TROPICAL_RAIN_FOREST)
+    arcade.schedule(on_update, 1 / 60)
 
     # Override arcade window methods
     window = arcade.get_window()
+    window.on_collision = on_collision
     window.on_draw = on_draw
     window.on_key_press = on_key_press
     window.on_key_release = on_key_release
